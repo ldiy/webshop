fa641e926335d22fb8148a6ef0b93821bc4f4dc1
L-diy <ldiy@users.noreply.github.com>
2022-10-15 19:37:23 +0200

    Improve the global exception and error handler

--- index.php
+++ index.php
@@ -13,21 +13,29 @@ spl_autoload_register(function ($class_name) {
 // Load helper file
 require_once 'Core/helpers.php';
 
+// Get the config variables from the config file
+$config = require_once 'App/config.php';
+
 // Set the global error and exception handlers
 // All exceptions should be already handled by the Kernel
 // But if something goes terribly wrong, we can still catch it here
 set_exception_handler(function (Throwable $e) {
-    echo '500: Internal Server Error';
+    global $config;
+    if (isset($config['debug']) && $config['debug']) {
+        echo 'message: ' . $e->getMessage() . '<br>';
+        echo 'file: ' . $e->getFile() . '<br>';
+        echo 'line: ' . $e->getLine() . '<br>';
+        echo 'trace: ' . $e->getTraceAsString() . '<br>';
+    } else {
+        echo 'Something went wrong';
+    }
     die();
 });
 
 set_error_handler(function ($errno, $errstr, $errfile, $errline) {
-    echo '500: Internal Server Error';
-    die();
+    throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
 });
 
-// Get the config variables from the config file
-$config = require_once 'App/config.php';
 
 // Create the kernel
 $kernel = new Kernel($config);
